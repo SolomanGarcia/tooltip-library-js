0cc58a80a5b777a0fae8a09901faecf1bc9374eb
Soloman Garcia <soloman.garcia@gmail.com>
2021-02-12 09:43:47 -0700

    create createToolTipElement function

--- tooltip.js
+++ tooltip.js
@@ -1,1 +1,17 @@
 import addGlobalEventListener from "./utils/addGlobalEventListener.js";
+
+// hover over a tooltip
+addGlobalEventListener("mouseover", "[data-tooltip]", (e) => {
+  const tooltip = createTooltipElement(e.target.dataset.tooltip);
+  document.body.append(tooltip);
+});
+
+// show tooltip over the top of the element
+// when mouse moves off the element remove the tooptip
+
+function createTooltipElement(text) {
+  const tooltip = document.createElement("div");
+  tooltip.classList.add("tooltip");
+  tooltip.innerText = text;
+  return tooltip;
+}
