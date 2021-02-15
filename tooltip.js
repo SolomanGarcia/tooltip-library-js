3224e9a0381153dbba2995406d1ca64627ce1198
Soloman Garcia <soloman.garcia@gmail.com>
2021-02-14 17:27:31 -0700

    add spacing

--- tooltip.js
+++ tooltip.js
@@ -1,5 +1,7 @@
 import addGlobalEventListener from "./utils/addGlobalEventListener.js";
 
+const DEFAULT_SPACING = 5;
+
 const tooltipContainer = document.createElement("div");
 tooltipContainer.classList.add("tooltip-container");
 document.body.append(tooltipContainer);
@@ -28,34 +30,36 @@ function createTooltipElement(text) {
 function positionTooltip(tooltip, element) {
   const elementRect = element.getBoundingClientRect();
   const tooltipRect = tooltip.getBoundingClientRect();
+  const spacing = parseInt(element.dataset.spacing) || DEFAULT_SPACING;
 
-  tooltip.style.top = `${elementRect.top - tooltipRect.height}px`;
+  tooltip.style.top = `${elementRect.top - tooltipRect.height - spacing}px`;
   tooltip.style.left = `${
     elementRect.left + elementRect.width / 2 - tooltipRect.width / 2
   }px`;
 
-  const bounds = isOutOfBounds(tooltip);
+  const bounds = isOutOfBounds(tooltip, spacing);
+
   if (bounds.top) {
     resetTooltipPosition(tooltip);
   }
   if (bounds.right) {
-    tooltip.style.right = 0;
+    tooltip.style.right = `${spacing}px`;
     tooltip.style.left = "initial";
   }
   if (bounds.left) {
-    tooltip.style.left = 0;
+    tooltip.style.left = `${spacing}px`;
   }
 }
 
-function isOutOfBounds(element) {
+function isOutOfBounds(element, spacing) {
   const rect = element.getBoundingClientRect();
   const containerRect = tooltipContainer.getBoundingClientRect();
 
   return {
-    left: rect.left <= containerRect.left,
-    right: rect.right >= containerRect.right,
-    top: rect.top <= containerRect.top,
-    bottom: rect.bottom >= containerRect.bottom
+    left: rect.left <= containerRect.left + spacing,
+    right: rect.right >= containerRect.right - spacing,
+    top: rect.top <= containerRect.top + spacing,
+    bottom: rect.bottom >= containerRect.bottom - spacing
   };
 }
 
